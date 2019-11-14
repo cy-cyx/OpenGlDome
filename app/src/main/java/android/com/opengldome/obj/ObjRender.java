78ff4d1e7f9c5d9c4e586b3e59e71207a55aa626
cyx <caiyx@adnonstop.com>
2019-11-14 18:02:00 +0800

    m加自动旋转

--- app/src/main/java/android/com/opengldome/obj/ObjRender.java
+++ app/src/main/java/android/com/opengldome/obj/ObjRender.java
@@ -27,6 +27,9 @@ public class ObjRender implements GLSurfaceView.Renderer {
     private float[] fData; // 点+纹理+法线
     private FloatBuffer bData;
 
+    // 眼睛的位置
+    private float[] sEyeLocal = new float[]{-0.f, -6.f, 1.f, 1.f};
+
     private float[] mvpMatrix = new float[16];
 
     private int mTexture;
@@ -52,13 +55,6 @@ public class ObjRender implements GLSurfaceView.Renderer {
         mHeight = height;
 
         mTexture = CommonUtils.newTexture(1, BitmapFactory.decodeResource(Application.getInstance().getResources(), R.drawable.dog_diffuse));
-
-        float ratio = (float) width / height;
-        float[] mProjectionMatrix = new float[16];
-        float[] mViewMatrax = new float[16];
-        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 4f, 10f);
-        Matrix.setLookAtM(mViewMatrax, 0, -3.f, -6.f, 2.0f, 0f, 0f, 0f, 0f, 0.0f, 1.0f);
-        Matrix.multiplyMM(mvpMatrix, 0, mProjectionMatrix, 0, mViewMatrax, 0);
     }
 
     @Override
@@ -83,8 +79,43 @@ public class ObjRender implements GLSurfaceView.Renderer {
         GLES30.glEnableVertexAttribArray(vCoord);
 
         GLES30.glUniform1i(uTexture, 1);
-        
+
+        upDataEye();
+        initMvpMatrix();
         GLES30.glUniformMatrix4fv(uMatrix, 1, false, mvpMatrix, 0);
         GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, fData.length / 5 * 3);
     }
+
+    private void initMvpMatrix() {
+        float ratio = (float) mWidth / mHeight;
+
+        float[] mProjectionMatrix = new float[16];
+        float[] mViewMatrax = new float[16];
+
+        //设置透视投影
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 4f, 20f);
+        //设置相机位置
+        Matrix.setLookAtM(mViewMatrax, 0, sEyeLocal[0], sEyeLocal[1], sEyeLocal[2], 0f, 0f, 0f, 0f, 0f, 1f);
+        //计算变换矩阵
+        Matrix.multiplyMM(mvpMatrix, 0, mProjectionMatrix, 0, mViewMatrax, 0);
+    }
+
+    private int mAngle = 180;
+
+    private float i = 0; // 控速
+
+    private void upDataEye() {
+        i++;
+        if (i < 3) return;
+        i = 0;
+
+        mAngle += 1;
+        if (mAngle == 360) {
+            mAngle = 0;
+        }
+        double b = Math.toRadians(mAngle);
+        sEyeLocal[0] = (float) (6f * Math.sin(b));
+        sEyeLocal[1] = (float) (6f * Math.cos(b));
+    }
+
 }
