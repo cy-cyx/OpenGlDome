4545e3305d2c81b5e0a4d08676ec0837499e9422
cyx <13202753052@163.com>
2019-11-19 01:59:14 +0800

    partics

--- app/src/main/java/android/com/opengldome/particsystem/PSRender.java
+++ app/src/main/java/android/com/opengldome/particsystem/PSRender.java
@@ -3,6 +3,7 @@ package android.com.opengldome.particsystem;
 import android.com.opengldome.Application;
 import android.com.opengldome.R;
 import android.com.opengldome.utils.CommonUtils;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
 
@@ -25,8 +26,9 @@ public class PSRender implements GLSurfaceView.Renderer {
     private int vEndPoint;
     private int vContinuedTime;
     private int uCurTime;
+    private int uTexture;
 
-    private int mPointSum = 500;
+    private int mPointSum = 1000;
     private int mTime = 600;   // 设置持续时间  GlSurfaceView 大概刷新时间1S 60次 最长10s
     private int mCurTime = Integer.MAX_VALUE;
 
@@ -46,6 +48,9 @@ public class PSRender implements GLSurfaceView.Renderer {
         vEndPoint = GLES30.glGetAttribLocation(mProgramObject, "vEndPoint");
         vContinuedTime = GLES30.glGetAttribLocation(mProgramObject, "vContinuedTime");
         uCurTime = GLES30.glGetUniformLocation(mProgramObject, "uCurTime");
+        uTexture = GLES30.glGetUniformLocation(mProgramObject, "uTexture");
+
+        CommonUtils.newTexture(1, BitmapFactory.decodeResource(Application.getInstance().getResources(), R.drawable.partics));
     }
 
     @Override
@@ -58,8 +63,13 @@ public class PSRender implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl) {
         GLES30.glViewport(0, 0, mWidth, mHeight);
 
+        GLES30.glEnable(GLES30.GL_TEXTURE_2D);
+
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
-        GLES30.glClearColor(1.f, 1.f, 1.f, 1.f);
+        GLES30.glClearColor(0.f, 0.f, 0.f, 1.f);
+
+        GLES30.glEnable(GLES30.GL_BLEND);
+        GLES30.glBlendFunc(GLES30.GL_SRC_ALPHA, GLES30.GL_ONE);
 
         // 更新数据
         upData();
@@ -76,18 +86,19 @@ public class PSRender implements GLSurfaceView.Renderer {
         GLES30.glEnableVertexAttribArray(vContinuedTime);
 
         GLES30.glUniform1f(uCurTime, mCurTime);
+        GLES30.glUniform1i(uTexture, 1);
 
         GLES30.glDrawArrays(GLES30.GL_POINTS, 0, mPointSum);
     }
 
-    public void upData() {
+    private void upData() {
 
         if (mCurTime > mTime) {
             mCurTime = 0;
-            // 设置点开始区域（x -0.05~0.05 y -0.5~-0.4 z -0.05~0.05）
+            // 设置点开始区域（x -0.05~0.05 y -0.6~-0.5 z -0.05~0.05）
             for (int i = 0; i < mPointSum; i++) {
                 mPointStart[3 * i] = (float) (-0.05f + new Random().nextInt(1000) / 1000f * .1f);
-                mPointStart[1 + 3 * i] = (float) (-0.5f + new Random().nextInt(1000) / 1000f * .1f);
+                mPointStart[1 + 3 * i] = (float) (-0.6f + new Random().nextInt(1000) / 1000f * .1f);
                 mPointStart[2 + 3 * i] = (float) (-0.05f + new Random().nextInt(1000) / 1000f * .1f);
             }
             bPointStart = CommonUtils.fToB(mPointStart);
--- app/src/main/res/raw/partics_vert.glsl
+++ app/src/main/res/raw/partics_vert.glsl
@@ -6,11 +6,15 @@ in float vContinuedTime;
 
 uniform float uCurTime;
 
+out float vert_continueTime;
+
 void main() {
     if (uCurTime <= vContinuedTime){
         gl_Position = vStartPoint + (vEndPoint - vStartPoint) * uCurTime / vContinuedTime;
-        gl_PointSize = 15.0f - 8.f * uCurTime / vContinuedTime;
+        gl_PointSize = 50.0f - 30.f * uCurTime / vContinuedTime;
     } else {
         gl_Position = vec4(-1000, -1000, 0, 0);
     }
+
+    vert_continueTime = vContinuedTime;
 }
